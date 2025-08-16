1f8bb5fea782d5469b312236dde59d420f5dd9ea
whogetzit <brian@triplicate.co>
2025-08-15 19:05:47 -0500

    Rename zip.ts to lib/zip.ts

--- zip.ts
+++
@@ -1,1 +0,0 @@
-import zipcodes from 'zipcodes'; export function zipToLatLon(zip:string){ const z:any=zipcodes.lookup(zip); if(!z) throw new Error('Invalid ZIP'); return { lat:z.latitude, lon:z.longitude }; }
