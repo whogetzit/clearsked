245aeb0f88fe511b6da2d45df7848df1b7a1f510
whogetzit <brian@triplicate.co>
2025-08-16 20:04:04 -0500

    Create db.ts

---
+++ server/db.ts
@@ -0,0 +1,12 @@
+// server/db.ts
+import { PrismaClient } from "@prisma/client";
+
+const g = globalThis as unknown as { prisma?: PrismaClient };
+
+export const prisma =
+  g.prisma ??
+  new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") g.prisma = prisma;
